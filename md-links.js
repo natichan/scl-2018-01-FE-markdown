c4325217876344583c77af381af56fd5a7149211
Natalia <nataliamh.88@gmail.com>
2018-08-24 16:50:53 -0300

    separadas funciones,, ejecuta en testingPath

--- md-links.js
+++ md-links.js
@@ -4,12 +4,19 @@ module.exports = {
 
 const path = require('path'); // console.log(path);
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 function testingPath(a){
     const absolutePath = path.resolve(a); //convierte la ruta a absoluta
-    fs.readFile( absolutePath, 'utf-8', (err, data) => {
+    readCompleteFile(absolutePath);
+    //console.log(absolutePath) 
+}
+function readCompleteFile (a){
+    fs.readFile( a, 'utf-8', (err, data) => {
         if(err) throw err;
         console.log(data);   
     })
-    console.log(absolutePath) 
 }
+/* fetch('https://www.google.cl/').then((response) => {
+    console.log(response);
+}) */
