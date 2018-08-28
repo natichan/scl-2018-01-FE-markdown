997e45d271ee102774418f84b18a453c1d6307f3
Natalia <nataliamh.88@gmail.com>
2018-08-28 13:11:47 -0300

    Creando tests

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,17 +1,5 @@
-/* Ejemplo de test jest
-const sum = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
-
-});
- */
 const sum = require('../md-links');
 
-test('muestra ruta al darle un parametro', () => {
-  expect(testingPath(hello).toBe(true));
+test('testingPath() convierte en absoluta la ruta del archivo entregada', () => {
+  expect(testingPath(README.md).toBe());
 });
