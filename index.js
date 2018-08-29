a4880eb6b382583a481f0a8272b508554e57b520
Natalia <nataliamh.88@gmail.com>
2018-08-29 13:27:01 -0300

    funciones por separado sin iniciar otra en ella

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let mdlinks = require('./md-links');
 // grab provided args
 let [,, ...args] = process.argv;
 
-mdlinks.validateTypeMarkdownFile(args[0]);
+mdlinks.readCompleteFile(args[0]);
--- md-links.js
+++ md-links.js
@@ -2,48 +2,52 @@ const path = require('path');
 const fs = require('fs');
 const marked = require('marked');
 const fetch = require('node-fetch');
-const color = require('colors');
 const Marked = require('marked');
 
-function validateTypeMarkdownFile(pathFile) {
-  const filesAllow = '.md'; // declaro archivos permitidos
-  const extension = (pathFile.substring(pathFile.lastIndexOf('.')).toLowerCase()); // divide para comprobar desde el punto en adelante el tipo de extension
-  if (filesAllow !== extension) {
-    console.log('Solo son permitidos archivos de tipo' + filesAllow);
-  } else {
-    // console.log('Archivo permitido');
-    return convertToAbsolutePath(pathFile);
-  }
-}
-function convertToAbsolutePath(pathFile) {
-  return new Promise((resolve, reject) => {
-    if (pathFile === '') reject('Debes ingresar un archivo');
-    if (resolve) 
-      absolutePath = path.resolve(pathFile); // convierte la ruta a absoluta
-    return readCompleteFile(absolutePath);
-  }); 
-}
+// function validateTypeMarkdownFile(pathFile) {
+//   const filesAllow = '.md'; // declaro archivos permitidos
+//   const extension = (pathFile.substring(pathFile.lastIndexOf('.')).toLowerCase()); // divide para comprobar desde el punto en adelante el tipo de extension
+//   if (filesAllow !== extension) {
+//     console.log('Solo son permitidos archivos de tipo markdown(md)');
+//   } else {
+//     console.log('Archivo permitido');
+//     return true;
+//   }
+// }
+// function convertToAbsolutePath(pathFile) {
+//   absolutePath = path.resolve(pathFile); // convierte la ruta a absoluta
+//   console.log(absolutePath);
+//   return absolutePath;
+// }; 
 function readCompleteFile(pathFile) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(pathFile, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-      let dataLine = data.split('\n');
-      // console.log(dataLine);
-      let dataExtractLine = dataLine.map(element => {
-      // console.log(element);
-        const linePosition = (dataLine.indexOf(element) + 1);
-        // console.log(dataLine.indexOf(element) + 1);
-        return markdownLinkExtractor(pathFile, element, linePosition);    
-      });  
-      dataExtractLine = dataExtractLine.filter(element => element.length !== 0);
-      dataExtractLine = dataExtractLine.reduce((elementOne, elementTwo) => elementOne.concat(elementTwo));
-      console.log(dataExtractLine);
+  fs.readFile(pathFile, 'utf-8', (err, data) => {
+    let dataLine = data.split('\n');
+    // console.log(dataLine);
+    let dataExtractLine = dataLine.map(element => {
+    // console.log(element);
+      const linePosition = (dataLine.indexOf(element) + 1);
+      // console.log(dataLine.indexOf(element) + 1);
+      return markdownLinkExtractor(pathFile, element, linePosition);    
+    });  
+    dataExtractLine = dataExtractLine.filter(element => 
+      element.length !== 0
+    );
+    dataExtractLine = dataExtractLine.reduce((elementOne, elementTwo) => 
+      elementOne.concat(elementTwo)
+    );
+    // console.log(dataExtractLine);
+    dataExtractLine.forEach(element => {
+      line = {
+        href: element.href,
+        text: element.text,
+        line: element.line,
+        path: element.path
+      };
+      console.log(Object.values(line));
     });
+    return dataExtractLine;
   });
-}
+};
 function markdownLinkExtractor(pathFile, markdown, position) {
   const links = [];
   const renderer = new Marked.Renderer();
@@ -72,28 +76,29 @@ function markdownLinkExtractor(pathFile, markdown, position) {
       title: title,
       line: position,
       path: pathFile
-
     });
   };
   Marked(markdown, {renderer: renderer});
-  // validateLink(links);
+  validateLink(links)
   // console.log(links);
   return links;
 };
-
 function validateLink(links) {
   links.forEach(element => {
     let url = element.href;
     fetch(url).then(response => response
     ).then(data => {
-      /*  console.log(data.url.blue);
-      console.log(data.status);
-      console.log(data.statusText.yellow); */
+      status = {
+        status: data.status,
+        statusText: data.statusText
+      };
+      console.log(Object.values(status));
+    
     }).catch(error => {
       console.error('ERROR > ' + error.status);
     });
   });
 }
 module.exports = {
-  validateTypeMarkdownFile
+  readCompleteFile
 };
