a1c001fa5613d3c6952c9d38aa0cc2504857b5b9
Natalia <nataliamh.88@gmail.com>
2018-08-31 10:19:44 -0300

    borre comentados console.log

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ let [,, ...args] = process.argv;
 
 mdlinks.mdLinks(args[0]).then((links) => {
   links.forEach(element => {
-    console.log(element.href + ':' + element.line + ' ' + element.text + ' ' + element.path);
+    console.log(element.href + ':' + element.line + ' ' + element.text.subString(0, 50) + ' ' + element.path);
   });
 });
