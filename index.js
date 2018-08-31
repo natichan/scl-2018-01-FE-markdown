4afbbef89084b0a6c19cb1cc93af6e151f8ffa50
Natalia <nataliamh.88@gmail.com>
2018-08-31 17:22:54 -0300

    console.log de validate y datos requeridos

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let mdlinks = require('./lib/md-links');
 // grab provided args
 let [,, ...args] = process.argv;
 let options = {};
-
 if (args.includes('--validate')) options.validate = true;
 
 mdlinks.mdLinks(args[0], options).then((links) => { 
   if (options.validate) {
-    console.log(links);
+    links.forEach(element => {
+      console.log(element.path + ':' + element.line + ' ' + element.href + ' ' + element.status + ' ' + element.statusTwo + ' ' + element.text);
+    });
+    // console.log(links);
   } else { 
     links.forEach(element => {
       console.log(element.path + ':' + element.line + ' ' + element.href + ' ' + element.text);
