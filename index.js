98d6918becc6ea483f8cf2893ff788dafa84d925
Natalia <nataliamh.88@gmail.com>
2018-08-31 12:25:47 -0300

    incorporando -- validate

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 let mdlinks = require('./lib/md-links');
 // grab provided args
 let [,, ...args] = process.argv;
+let options = {};
 
-mdlinks.mdLinks(args[0]).then((links) => {
+if (args.includes('--validate')) options.validate = true;
+
+mdlinks.mdLinks(args[0], options).then((links) => {
   links.forEach(element => {
-    console.log(element.href + ':' + element.line + ' ' + element.text.subString(0, 50) + ' ' + element.path);
+    console.log(element.href + ':' + element.line + ' ' + element.text + ' ' + element.path);
+  }).catch((error) => {
+    console.log('Error >' + error);
   });
 });
