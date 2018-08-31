0fdf2614242d2c901e0e01b3f078495a8482cf28
Natalia <nataliamh.88@gmail.com>
2018-08-31 20:15:48 -0300

    probando para exportar como modulo

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
 let mdlinks = require('./lib/md-links');
-// grab provided args
-let [,, ...args] = process.argv;
-let options = {};
-if (args.includes('--validate')) options.validate = true;
 
-mdlinks.mdLinks(args[0], options).then((links) => {
-  if (options.validate) {
-    links.forEach(element => {
-      console.log(element.path.green + ':' + element.line + ' ' + element.href + ' ' + element.status + ' ' + element.statusTwo + ' ' + element.text);      
-    });
-  } else { 
-    links.forEach(element => {
-      console.log(element.path.green + ':' + element.line + ' ' + element.href + ' ' + element.text);
-    });
-  }
-}).catch((error) => {
-  console.error('Error >' + error);
-});
+if (require.main === module) {
+  let [,, ...args] = process.argv;
+  let options = {};
+  if (args.includes('--validate')) options.validate = true;
+
+  mdlinks.mdLinks(args[0], options).then((links) => {
+    if (options.validate) {
+      links.forEach(element => {
+        console.log(element.path.green + ':' + element.line + ' ' + element.href + ' ' + element.status + ' ' + element.statusTwo + ' ' + element.text);      
+      });
+    } else { 
+      links.forEach(element => {
+        console.log(element.path.green + ':' + element.line + ' ' + element.href + ' ' + element.text);
+      });
+    }
+  }).catch((error) => {
+    console.error('Error >' + error);
+  });
+}
+
+module.exports = mdLinks;
