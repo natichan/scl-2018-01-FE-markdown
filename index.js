5ed1216fe804ba44ed4e841de89ff105b81d6f58
Natalia <nataliamh.88@gmail.com>
2018-08-31 10:11:54 -0300

    funcion md links conectada conectada a leer archivo y extraer datos del archivo

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ let mdlinks = require('./lib/md-links');
 // grab provided args
 let [,, ...args] = process.argv;
 
-mdlinks.mdLinks(args[0]);
+mdlinks.mdLinks(args[0]).then((links) => {
+  links.forEach(element => {
+    console.log(element.href + ':' + element.line + ' ' + element.text + ' ' + element.path);
+  });
+});
