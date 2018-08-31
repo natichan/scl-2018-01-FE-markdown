1fcc33f02cbb22b4ef6546fdd92398f83a3a8ff9
Natalia <nataliamh.88@gmail.com>
2018-08-31 08:48:17 -0300

    agregue 2 tests

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,8 +2,9 @@ const mdLinks = require('../lib/md-links');
 
 describe('validateTypeMarkdownFile(pathFile)', () => {
   test('válida que el archivo sea tipo md', () => {
-    function validateTypeMarkdownFile(pathFile) {
-      expect((README.md).toBeTruthy());
-    }
+    expect(mdLinks.validateTypeMarkdownFile('README.md')).toBeTruthy();
+  });
+  test('válida que el archivo sea tipo md', () => {
+    expect(mdLinks.validateTypeMarkdownFile('app.js')).not.toBeTruthy();
   });
 });
