fff478691c89ad77d266aeca5cc1f579ab99da42
Natalia <nataliamh.88@gmail.com>
2018-09-05 12:13:34 -0300

    nuevo test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -18,3 +18,8 @@ describe('function convertToAbsolutePath(pathFile)', () => {
     expect(mdLinks.convertToAbsolutePath('README.md')).toBe('/home/laboratoria/Documents/Laboratoria/Proyectos/scl-2018-01-FE-markdown/README.md');
   });
 });
+describe('function processAndExtractFile(pathFile)', () => {
+  test('lee el archivo entregado', () => {
+    return expect(mdLinks.processAndExtractFile('README.md')).resolves.toBeTruthy();
+  });
+});
